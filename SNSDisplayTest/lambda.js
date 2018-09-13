d9c6bc3ca06ebf4592cf63a5bfd388347af4c221
sigmaidetest <cehib@balanc3r.com>
2018-09-13 14:54:42 +0530

    init

---
+++ SNSDisplayTest/lambda.js
@@ -0,0 +1,20 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
+exports.handler = function (event, context, callback) {
+
+    sns.publish({
+        Message: 'Hello',
+        MessageAttributes: {},
+        MessageStructure: 'String',
+        TopicArn: 'arn:aws:sns:us-east-1:318300609668:SigmaTopic'
+    }).promise()
+        .then(data => {
+            // your code goes here
+        })
+        .catch(err => {
+            // error handling goes here
+        });
+
+    callback(null, 'Successfully executed');
+}
